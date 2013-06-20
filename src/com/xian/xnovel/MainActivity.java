7aa3a24183cf38b1ed4ba207669bf4960749c3b1
limingfeng <limingfeng@maritimus.(none)>
2013-06-20 10:02:34 +0800

    init

---
+++ src/com/xian/xnovel/MainActivity.java
@@ -0,0 +1,117 @@
+package com.xian.xnovel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.xian.xnovel.R;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.TextView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.NavUtils;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+
+public class MainActivity extends FragmentActivity {
+	private final int TABS_COUNT = 5;
+	private int currIndex = 0;
+
+	private ViewPager mPager;
+	private List<Fragment> fragmentsList;
+	private List<TextView> tabsList;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_main);
+		initView();
+		initData();
+	}
+
+	private void initView() {
+		tabsList = new ArrayList<TextView>(TABS_COUNT);
+		tabsList.add((TextView) findViewById(R.id.tab_btn_category));
+		tabsList.add((TextView) findViewById(R.id.tab_btn_bookmark));
+		tabsList.add((TextView) findViewById(R.id.tab_btn_history));
+		tabsList.add((TextView) findViewById(R.id.tab_btn_setting));
+		tabsList.add((TextView) findViewById(R.id.tab_btn_more));
+
+		for (int i = 0; i < TABS_COUNT; i++) {
+			tabsList.get(i).setOnClickListener(new MyOnClickListener(i));
+		}
+
+		InitViewPager();
+	}
+
+	private void initData() {
+		currIndex = 0;
+		tabsList.get(0).setSelected(true);
+	}
+
+	private void InitViewPager() {
+		mPager = (ViewPager) findViewById(R.id.main_body_pager);
+		fragmentsList = new ArrayList<Fragment>(TABS_COUNT);
+		LayoutInflater mInflater = getLayoutInflater();
+		View activityView = mInflater.inflate(R.layout.fragment_lay, null);
+
+		Fragment activityfragment = TestFragment.newInstance("目录");
+		Fragment groupFragment = TestFragment.newInstance("书签");
+		Fragment friendsFragment = TestFragment.newInstance("历史");
+		Fragment chatFragment = TestFragment.newInstance("设置");
+		Fragment aboutFragment = TestFragment.newInstance("关于");
+
+		fragmentsList.add(activityfragment);
+		fragmentsList.add(groupFragment);
+		fragmentsList.add(friendsFragment);
+		fragmentsList.add(chatFragment);
+		fragmentsList.add(aboutFragment);
+
+		mPager.setAdapter(new MyFragmentPagerAdapter(
+				getSupportFragmentManager(), fragmentsList));
+		mPager.setCurrentItem(0);
+		mPager.setOnPageChangeListener(new MyOnPageChangeListener());
+	}
+
+	public class MyOnPageChangeListener implements OnPageChangeListener {
+
+		@Override
+		public void onPageSelected(int arg0) {
+
+			tabsList.get(currIndex).setSelected(false);
+			tabsList.get(arg0).setSelected(true);
+			currIndex = arg0;
+		}
+
+		@Override
+		public void onPageScrolled(int arg0, float arg1, int arg2) {
+		}
+
+		@Override
+		public void onPageScrollStateChanged(int arg0) {
+		}
+	}
+
+	public class MyOnClickListener implements View.OnClickListener {
+		private int index = 0;
+
+		public MyOnClickListener(int i) {
+			index = i;
+		}
+
+		@Override
+		public void onClick(View v) {
+			mPager.setCurrentItem(index);
+		}
+	};
+
+}
---
+++ src/com/xian/xnovel/MyFragmentPagerAdapter.java
@@ -0,0 +1,40 @@
+package com.xian.xnovel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.ViewGroup;
+
+public class MyFragmentPagerAdapter extends FragmentPagerAdapter {
+	private List<Fragment> fragmentsList;
+
+	public MyFragmentPagerAdapter(FragmentManager fm) {
+		super(fm);
+	}
+
+	public MyFragmentPagerAdapter(FragmentManager fm, List<Fragment> fragments) {
+		super(fm);
+		this.fragmentsList = fragments;
+	}
+
+	@Override
+	public int getCount() {
+		return fragmentsList.size();
+	}
+
+	@Override
+	public Fragment getItem(int arg0) {
+		return fragmentsList.get(arg0);
+	}
+
+	@Override
+	public int getItemPosition(Object object) {
+		return super.getItemPosition(object);
+	}
+
+
+}
