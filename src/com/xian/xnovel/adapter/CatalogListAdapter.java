4f419a7f50160d8fc9e15f3c1b8a36000dbb2f73
lucien <lucien.one@gmail.com>
2014-03-23 23:41:24 +0800

    error

--- src/com/xian/xnovel/adapter/CatalogListAdapter.java
+++ src/com/xian/xnovel/adapter/CatalogListAdapter.java
@@ -1,117 +1,132 @@
+
 package com.xian.xnovel.adapter;
 
-import java.util.List;
-
-import com.xian.xnovel.R;
-import com.xian.xnovel.R.id;
-import com.xian.xnovel.R.layout;
-import com.xian.xnovel.domain.CatalogInfo;
-
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.ImageView;
 import android.widget.SectionIndexer;
 import android.widget.TextView;
 
+import com.xian.xnovel.R;
+import com.xian.xnovel.domain.CatalogInfo;
+
+import java.util.List;
+
 public class CatalogListAdapter extends BaseAdapter implements SectionIndexer {
 
-	private LayoutInflater mInflater;
-	private List<CatalogInfo> dataList;
-	private Context mContext;
-	private Integer[] indexs;
+    private final LayoutInflater mInflater;
+    private List<CatalogInfo> dataList;
+    private final int titleCount;
 
-	public CatalogListAdapter(Context context, List<CatalogInfo> list) {
-		this.mInflater = LayoutInflater.from(context);
-		dataList = list;
-		mContext = context;
-		initTitleIndex();
-	}
+    // private Context mContext;
+    private Integer[] indexs;
 
-	private void initTitleIndex() {
-		if (dataList != null) {
-			indexs = new Integer[dataList.size()];
+    public CatalogListAdapter(Context context, List<CatalogInfo> list, int count) {
+        this.mInflater = LayoutInflater.from(context);
+        dataList = list;
+        titleCount = count;
+        // mContext = context;
+        initTitleIndex();
+    }
 
-			for(int i=0;i<indexs.length;i++)
-				indexs[i] = i+1;
+    private void initTitleIndex() {
+        if (dataList != null) {
+            indexs = new Integer[dataList.size()];
 
-		}
-	}
+            for (int i = 0; i < indexs.length; i++)
+                indexs[i] = i + 1;
 
-	@Override
-	public int getCount() {
-		// TODO Auto-generated method stub
-		return dataList.size();
-	}
+        }
+    }
 
-	@Override
-	public Object getItem(int arg0) {
-		// TODO Auto-generated method stub
-		return arg0;
-	}
+    @Override
+    public int getCount() {
+        // TODO Auto-generated method stub
+        return dataList.size();
+    }
 
-	@Override
-	public long getItemId(int arg0) {
-		// TODO Auto-generated method stub
-		return arg0;
-	}
+    @Override
+    public Object getItem(int arg0) {
+        // TODO Auto-generated method stub
+        return arg0;
+    }
 
-	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
-		ViewHolder holder = null;
-		if (convertView == null) {
-			holder = new ViewHolder();
-			convertView = mInflater.inflate(R.layout.list_cata_item, null);
-			holder.title = (TextView) convertView
-					.findViewById(R.id.list_cata_title);
-			holder.content1 = (TextView) convertView
-					.findViewById(R.id.list_cata_content1);
-			holder.content2 = (TextView) convertView
-					.findViewById(R.id.list_cata_content2);
-			convertView.setTag(holder);
-		} else {
-			holder = (ViewHolder) convertView.getTag();
-		}
-		CatalogInfo info = dataList.get(position);
+    @Override
+    public long getItemId(int arg0) {
+        // TODO Auto-generated method stub
+        return arg0;
+    }
 
-		holder.title.setText(info.getTitle());
-		String[] strs = info.getContent().split(" ");
-		holder.content1.setText(strs[0]);
-		holder.content2.setText(strs[1]);
-		return convertView;
-	}
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder = null;
+        if (convertView == null) {
+            holder = new ViewHolder();
+            convertView = mInflater.inflate(R.layout.list_cata_item, null);
+            holder.titleLeftTv = (TextView) convertView
+                    .findViewById(R.id.list_cata_title_left);
+            holder.titleCenterTv = (TextView) convertView
+                    .findViewById(R.id.list_cata_title_center);
+            holder.titleRightTv = (TextView) convertView
+                    .findViewById(R.id.list_cata_title_right);
 
-	public final class ViewHolder {
-		public TextView title;
-		public TextView content1;
-		public TextView content2;
-	}
+            if (titleCount == 1) {
+                holder.titleLeftTv.setVisibility(View.GONE);
+                holder.titleRightTv.setVisibility(View.GONE);
+            } else if (titleCount == 2) {
+                holder.titleCenterTv.setVisibility(View.GONE);
+            }
 
-	@Override
-	public int getPositionForSection(int section) {
-		return section;
-	}
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+        CatalogInfo info = dataList.get(position);
+        String[] strs = info.getTitles().split(",");
+        if (titleCount == 1) {
+            holder.titleCenterTv.setText(strs[0]);
 
-	@Override
-	public int getSectionForPosition(int position) {
-		return position;
-	}
+        } else if (titleCount == 2) {
+            holder.titleLeftTv.setText(strs[0]);
+            holder.titleRightTv.setText(strs[1]);
+        } else {
+            holder.titleLeftTv.setText(strs[0]);
+            holder.titleCenterTv.setText(strs[1]);
+            holder.titleRightTv.setText(strs[2]);
+        }
+        return convertView;
+    }
 
-	@Override
-	public Object[] getSections() {
-		return indexs;
-	}
+    public final class ViewHolder {
+        public TextView titleLeftTv;
+        public TextView titleCenterTv;
+        public TextView titleRightTv;
+    }
 
-	public List<CatalogInfo> getDataList() {
-		return dataList;
-	}
+    @Override
+    public int getPositionForSection(int section) {
+        return section;
+    }
 
-	public void setDataList(List<CatalogInfo> dataList) {
-		this.dataList = dataList;
-		initTitleIndex();
-	}
+    @Override
+    public int getSectionForPosition(int position) {
+        return position;
+    }
+
+    @Override
+    public Object[] getSections() {
+        return indexs;
+    }
+
+    public List<CatalogInfo> getDataList() {
+        return dataList;
+    }
+
+    public void setDataList(List<CatalogInfo> dataList) {
+        this.dataList = dataList;
+        initTitleIndex();
+    }
 
 }
--- src/com/xian/xnovel/adapter/MarkListAdapter.java
+++ src/com/xian/xnovel/adapter/MarkListAdapter.java
@@ -1,18 +1,7 @@
+
 package com.xian.xnovel.adapter;
 
-import java.sql.Date;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.List;
-
-import com.xian.xnovel.R;
-import com.xian.xnovel.R.id;
-import com.xian.xnovel.R.layout;
-import com.xian.xnovel.domain.CatalogInfo;
-import com.xian.xnovel.domain.MarkInfo;
-
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,94 +9,118 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.xian.xnovel.R;
+import com.xian.xnovel.domain.MarkInfo;
+
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
 public class MarkListAdapter extends BaseAdapter {
 
-	private LayoutInflater mInflater;
-	private List<MarkInfo> dataList;
-	private SimpleDateFormat dateFormatter;
-	private SimpleDateFormat timeFormatter;
+    private final LayoutInflater mInflater;
+    private List<MarkInfo> dataList;
+    private final SimpleDateFormat dateFormatter;
+    private final SimpleDateFormat timeFormatter;
+    private final int titleCount;
 
-	public MarkListAdapter(Context context, List<MarkInfo> list) {
-		this.mInflater = LayoutInflater.from(context);
-		dataList = list;
-		dateFormatter = new SimpleDateFormat("yyyy-MM-dd");
-		timeFormatter = new SimpleDateFormat("HH:mm:ss");
+    public MarkListAdapter(Context context, List<MarkInfo> list, int count) {
+        this.mInflater = LayoutInflater.from(context);
+        dataList = list;
+        dateFormatter = new SimpleDateFormat("yyyy-MM-dd");
+        timeFormatter = new SimpleDateFormat("HH:mm:ss");
+        titleCount = count;
 
-	}
+    }
 
-	@Override
-	public int getCount() {
-		// TODO Auto-generated method stub
-		return dataList.size();
-	}
+    @Override
+    public int getCount() {
+        // TODO Auto-generated method stub
+        return dataList.size();
+    }
 
-	@Override
-	public Object getItem(int arg0) {
-		// TODO Auto-generated method stub
-		return arg0;
-	}
+    @Override
+    public Object getItem(int arg0) {
+        // TODO Auto-generated method stub
+        return arg0;
+    }
 
-	@Override
-	public long getItemId(int arg0) {
-		// TODO Auto-generated method stub
-		return arg0;
-	}
+    @Override
+    public long getItemId(int arg0) {
+        // TODO Auto-generated method stub
+        return arg0;
+    }
 
-	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
-		ViewHolder holder = null;
-		if (convertView == null) {
-			holder = new ViewHolder();
-			convertView = mInflater.inflate(R.layout.list_mark_item, null);
-			holder.iconIv = (ImageView) convertView
-					.findViewById(R.id.list_mark_iv_head);
-			holder.positionTv = (TextView) convertView
-					.findViewById(R.id.list_mark_tv_position);
-			holder.dateTv = (TextView) convertView
-					.findViewById(R.id.list_mark_tv_date);
-			holder.titleTv = (TextView) convertView
-					.findViewById(R.id.list_mark_tv_title);
-			holder.content1Tv = (TextView) convertView
-					.findViewById(R.id.list_mark_tv_content1);
-			holder.content2Tv = (TextView) convertView
-					.findViewById(R.id.list_mark_tv_content2);
-			convertView.setTag(holder);
-		} else {
-			holder = (ViewHolder) convertView.getTag();
-		}
-		MarkInfo info = dataList.get(position);
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder = null;
+        if (convertView == null) {
+            holder = new ViewHolder();
+            convertView = mInflater.inflate(R.layout.list_mark_item, null);
+            holder.iconIv = (ImageView) convertView
+                    .findViewById(R.id.list_mark_iv_head);
+            holder.positionTv = (TextView) convertView
+                    .findViewById(R.id.list_mark_tv_position);
+            holder.dateTv = (TextView) convertView
+                    .findViewById(R.id.list_mark_tv_date);
+            holder.title1Tv = (TextView) convertView
+                    .findViewById(R.id.list_mark_tv_title1);
+            holder.title2Tv = (TextView) convertView
+                    .findViewById(R.id.list_mark_tv_title2);
+            holder.title3Tv = (TextView) convertView
+                    .findViewById(R.id.list_mark_tv_title3);
+            if (titleCount == 1) {
+                holder.title2Tv.setVisibility(View.GONE);
+                holder.title3Tv.setVisibility(View.GONE);
+            } else if (titleCount == 2) {
+                holder.title3Tv.setVisibility(View.GONE);
+            }
 
-		if (info.getType() == MarkInfo.TYPE_HISTORY) {
-			holder.iconIv.setImageResource(R.drawable.list_history_icon_selector);
-		} else {
-			holder.iconIv.setImageResource(R.drawable.list_mark_icon_selector);
-		}
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+        MarkInfo info = dataList.get(position);
 
-		holder.positionTv.setText(info.getPercent());
-		Date date=new Date(info.getDate());
-		holder.dateTv.setText(dateFormatter.format(date)+"\n"+timeFormatter.format(date));
-		holder.titleTv.setText(info.getTitle());
-		String[] strs = info.getContent().split(" ");
-		holder.content1Tv.setText(strs[0]);
-		holder.content2Tv.setText(strs[1]);
-		return convertView;
-	}
+        if (info.getType() == MarkInfo.TYPE_HISTORY) {
+            holder.iconIv.setImageResource(R.drawable.list_history_icon_selector);
+        } else {
+            holder.iconIv.setImageResource(R.drawable.list_mark_icon_selector);
+        }
 
-	public final class ViewHolder {
-		public ImageView iconIv;
-		public TextView positionTv;
-		public TextView dateTv;
-		public TextView titleTv;
-		public TextView content1Tv;
-		public TextView content2Tv;
-	}
+        holder.positionTv.setText(info.getPercent());
+        Date date = new Date(info.getDate());
+        holder.dateTv.setText(dateFormatter.format(date) + "\n" + timeFormatter.format(date));
+        String[] strs = info.getCatalog().getTitles().split(",");
+        if (titleCount == 1) {
+            holder.title1Tv.setText(strs[0]);
 
-	public List<MarkInfo> getDataList() {
-		return dataList;
-	}
+        } else if (titleCount == 2) {
+            holder.title1Tv.setText(strs[0]);
+            holder.title2Tv.setText(strs[1]);
+        } else {
+            holder.title1Tv.setText(strs[0]);
+            holder.title2Tv.setText(strs[1]);
+            holder.title3Tv.setText(strs[2]);
+        }
+        return convertView;
+    }
 
-	public void setDataList(List<MarkInfo> dataList) {
-		this.dataList = dataList;
-	}
+    public final class ViewHolder {
+        public ImageView iconIv;
+        public TextView positionTv;
+        public TextView dateTv;
+        public TextView title1Tv;
+        public TextView title2Tv;
+        public TextView title3Tv;
+    }
+
+    public List<MarkInfo> getDataList() {
+        return dataList;
+    }
+
+    public void setDataList(List<MarkInfo> dataList) {
+        this.dataList = dataList;
+    }
 
 }
